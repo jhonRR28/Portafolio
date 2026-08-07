2bfc31ca03e8b74712753ab5c8bcffc82c155563
jhonRR28 <rmales1728@hotmail.com>
2026-08-07 17:34:13 -0500

    Modificacion de las cards para la visualizacion desde un dispositivo movil

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -145,19 +145,19 @@ const Tech = () => {
       <div className="flex w-full flex-col items-center justify-center  p-5 md:p-14">
         {techCategories.map((category) => (
           <div key={category.title} className="mb-14">
-            <h2 className="mb-8 text-center text-2xl font-semibold bg-linear-to-r from-sky-300 via-cyan-400 to-blue-800 bg-clip-text text-transparent">
+            <h2 className="mb-8 text-center text-2xl font-light bg-linear-to-r from-sky-300 via-cyan-400 to-blue-800 bg-clip-text text-transparent md:font-semibold">
               {category.title}
             </h2>
-            <div className="flex flex-wrap justify-center gap-10">
+            <div className="flex flex-wrap justify-center gap-4 md:gap-10">
               {category.technologies.map((tech) => {
                 const Icon = tech.icon;
                 return (
                   <div
-                    key={tech.name} className="group w-38 flex flex-col items-center justify-center gap-4 rounded-2xl border border-slate-700/50 bg-slate-900/40 px-4 py-6 duration-300 hover:-translate-y-2 hover:border-sky-400/50 hover:shadow-lg hover:shadow-sky-500/20">
+                    key={tech.name} className="group w-32 flex flex-col items-center justify-center gap-2 rounded-2xl border border-slate-700/50 bg-slate-900/40 px-3 py-4 transition-all duration-300 hover:-translate-y-2 hover:border-sky-400/50 hover:shadow-lg hover:shadow-sky-500/20 md:w-38 md:gap-4 md:px-4 md:py-6">
                     <Icon
-                      className={`${tech.color} text-5xl transition-transform duration-300 group-hover:scale-110`}
+                      className={`${tech.color} text-4xl transition-transform duration-300 group-hover:scale-110 md:text-5xl`}
                     />
-                    <span className="text-center text-white">
+                    <span className="text-center text-sm text-white md:text-base">
                       {tech.name}
                     </span>
                   </div>
